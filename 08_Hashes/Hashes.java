086eb126ca6b928981eef02a8769335ec1c28f87
Juandiaz <juandica2005@gmail.com>
2024-11-13 20:49:13 +0100

    casi acabat

---
+++ 08_Hashes/Hashes.java
@@ -0,0 +1,82 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HexFormat;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class Hashes {
+
+    private int npass;
+
+    public String getSHA512AmbSalt(String pw, String salt) throws NoSuchAlgorithmException{
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes());
+            byte[] hashBytes = md.digest(pw.getBytes());
+            HexFormat hex = HexFormat.of();
+            String hash = hex.formatHex(hashBytes);
+            return hash;
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public String getPBKDF2AmbSalt(String pw, String salt)  throws NoSuchAlgorithmException, InvalidKeySpecException{
+        try {
+            int iteracions = 10000;
+            int clauLength = 128; 
+            char[] pwChar = pw.toCharArray();
+            byte[] saltBytes = salt.getBytes();
+
+            PBEKeySpec spec = new PBEKeySpec(pwChar, saltBytes, iteracions, clauLength);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHa512");
+            byte[] hashedBytes = keyFactory.generateSecret(spec).getEncoded();
+
+            HexFormat hex = HexFormat.of();
+            return hex.formatHex(hashedBytes);
+        }catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public String forcaBruta(String alg, String hash, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException{
+        char[] charset = "abcdefABCDEF1234567890!".toCharArray();
+        npass = 0;
+        /* codigo a implementar */
+        return "";
+    }
+
+    public String getInterval(long t1, long t2){
+        long diferencia = t2-t1;
+        return diferencia + "ms";
+
+    }
+    public static void main(String[] args) throws Exception{
+        String salt = "qpoweiruañslkdfjz";
+        String pw = "aaabF!";
+        Hashes h = new Hashes();
+        String[] aHashes = { h.getSHA512AmbSalt(pw, salt),
+        h.getPBKDF2AmbSalt(pw, salt) };
+        String pwTrobat = null;
+        String[] algorismes = {"SHA-512", "PBKDF2"};
+        for(int i=0; i< aHashes.length; i++){
+            System.out.printf("===========================\n");
+            System.out.printf("Algorisme: %s\n", algorismes[i]);
+            System.out.printf("Hash: %s\n",aHashes[i]);
+            System.out.printf("---------------------------\n");
+            System.out.printf("-- Inici de força bruta ---\n");
+            
+            long t1 = System.currentTimeMillis();
+            pwTrobat = h.forcaBruta(algorismes[i], aHashes[i], salt);
+            long t2 = System.currentTimeMillis();
+            
+            System.out.printf("Pass : %s\n", pwTrobat);
+            System.out.printf("Provats: %d\n", h.npass);
+            System.out.printf("Temps : %s\n", h.getInterval(t1, t2));
+            System.out.printf("---------------------------\n\n");
+        }
+    }
+
+}
